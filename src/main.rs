9cb1386ddd105a62bab4370b2b543f336c5986e2
Slava Barinov <rayslava@gmail.com>
2024-02-15 11:39:03 +0300

    Reqwest-based POST and GET added

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,95 @@
 use hyper::{
     service::{make_service_fn, service_fn},
-    Request, Response, Server,
+    Body, Method, Request, Response, Server, StatusCode,
 };
 use log::{error, info, warn};
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use tokio::io::{self, split};
 use tokio_socks::tcp::Socks5Stream;
 
-async fn handle_connect(req: Request<hyper::Body>) -> Result<Response<hyper::Body>, hyper::Error> {
-    let method = req.method().clone();
-    let uri = req.uri().clone();
+async fn proxy_http_through_socks5(
+    req: Request<Body>,
+    proxy_addr: SocketAddr,
+) -> Result<Response<Body>, hyper::Error> {
+    let client = reqwest::Client::builder()
+        .proxy(reqwest::Proxy::all(format!("socks5://{}", proxy_addr)).unwrap())
+        .build()
+        .unwrap();
 
-    if method != hyper::Method::CONNECT {
-        // Log non-CONNECT method request
-        warn!("Received non-CONNECT request: {} {}", method, uri);
-        return Ok(Response::new(hyper::Body::from("Method Not Allowed")));
-    }
+    let full_url = format!(
+        "http://{}{}",
+        req.uri().host().unwrap(),
+        req.uri().path_and_query().map_or("", |pq| pq.as_str())
+    );
+    let response = client
+        .post(&full_url)
+        .body(
+            hyper::body::to_bytes(req.into_body())
+                .await
+                .unwrap()
+                .to_vec(),
+        )
+        .send()
+        .await
+        .unwrap();
 
-    // Log the CONNECT request
-    info!("Handling CONNECT request for {}", uri);
+    let status = response.status();
+    let headers = response.headers().clone();
+    let body = hyper::Body::from(response.bytes().await.unwrap().to_vec());
 
-    let host = req.uri().authority().unwrap().to_string();
-    let proxy_addr = "127.0.0.1:12345".parse::<SocketAddr>().unwrap(); // SOCKS5 proxy address
+    let mut hyper_response = Response::new(body);
+    *hyper_response.status_mut() = StatusCode::from_u16(status.as_u16()).unwrap();
+    *hyper_response.headers_mut() = headers.clone();
 
-    // Immediately return a successful response for CONNECT method
-    tokio::spawn(async move {
-        if let Ok(upgraded) = hyper::upgrade::on(req).await {
-            match Socks5Stream::connect(proxy_addr, host).await {
-                Ok(mut proxy_stream) => {
-                    let (mut client_reader, mut client_writer) = split(upgraded);
-                    let (mut server_reader, mut server_writer) = proxy_stream.split();
+    Ok(hyper_response)
+}
 
-                    let client_to_server = io::copy(&mut client_reader, &mut server_writer);
-                    let server_to_client = io::copy(&mut server_reader, &mut client_writer);
+async fn handle_request(
+    req: Request<Body>,
+    proxy_addr: SocketAddr,
+) -> Result<Response<Body>, hyper::Error> {
+    match *req.method() {
+        Method::CONNECT => {
+            let uri = req.uri().clone();
+            info!("Handling CONNECT request for {}", uri);
 
-                    match tokio::try_join!(client_to_server, server_to_client) {
-                        Ok((_, _)) => info!("Tunnel closed successfully for {}", uri),
-                        Err(e) => error!("Error in tunneling for {}: {}", uri, e),
+            let host = req.uri().authority().unwrap().to_string();
+
+            tokio::spawn(async move {
+                if let Ok(upgraded) = hyper::upgrade::on(req).await {
+                    match Socks5Stream::connect(proxy_addr, host).await {
+                        Ok(mut proxy_stream) => {
+                            let (mut client_reader, mut client_writer) = tokio::io::split(upgraded);
+                            let (mut server_reader, mut server_writer) = proxy_stream.split();
+
+                            let client_to_server =
+                                tokio::io::copy(&mut client_reader, &mut server_writer);
+
+                            let server_to_client =
+                                tokio::io::copy(&mut server_reader, &mut client_writer);
+
+                            match tokio::try_join!(client_to_server, server_to_client) {
+                                Ok((_, _)) => info!("Tunnel closed successfully for {}", uri),
+                                Err(e) => error!("Error in tunneling for {}: {}", uri, e),
+                            }
+                        }
+                        Err(e) => error!(
+                            "Failed to connect to target through SOCKS5 for {}: {}",
+                            uri, e
+                        ),
                     }
                 }
-                Err(e) => {
-                    error!(
-                        "Failed to connect to target through SOCKS5 for {}: {}",
-                        uri, e
-                    );
-                }
-            }
-        }
-    });
+            });
 
-    Ok(Response::builder()
-        .status(200)
-        .body(hyper::Body::empty())
-        .unwrap())
+            Ok(Response::builder().status(200).body(Body::empty()).unwrap())
+        }
+        Method::POST => proxy_http_through_socks5(req, proxy_addr).await,
+        Method::GET => proxy_http_through_socks5(req, proxy_addr).await,
+        _ => {
+            warn!("Received unsupported request method");
+            Ok(Response::new(Body::from("Method Not Allowed")))
+        }
+    }
 }
 
 #[tokio::main]
@@ -61,9 +97,13 @@ async fn main() {
     env_logger::init();
 
     let addr = ([127, 0, 0, 1], 3129).into();
+    let proxy_addr = "127.0.0.1:12345"
+        .parse::<SocketAddr>()
+        .expect("Invalid SOCKS5 proxy address");
 
-    let make_svc =
-        make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle_connect)) });
+    let make_svc = make_service_fn(move |_conn| async move {
+        Ok::<_, Infallible>(service_fn(move |req| handle_request(req, proxy_addr)))
+    });
 
     let server = Server::bind(&addr).serve(make_svc);
 
